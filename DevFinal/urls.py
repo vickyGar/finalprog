f747fff03fb090fbc01fd87f687eb9c66cd0e570
Vicky <gcardonaalba@gmail.com>
2021-10-07 20:32:28 -0600

    exa

--- DevFinal/urls.py
+++ DevFinal/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('apps.web.urls', 'web')))
-]
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
